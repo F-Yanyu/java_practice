99486ad0c47dd6c33eb409a01a6764387e5fd885
付延余 <F-Yanyu@outlook.com>
2023-11-25 23:38:38 +0800

    studyJava

---
+++ src/main/java/org/example/project/Annotation/ProcessingAnnotation.java
@@ -0,0 +1,13 @@
+package org.example.project.Annotation;
+
+public class ProcessingAnnotation {
+    public static void main(String[] args){
+        /**
+         * @处理注解
+         */
+        int[] a = {1,2,3};
+        for (int i = 0; i < a.length ; i++) {
+            System.out.println(i);
+        }
+    }
+}
