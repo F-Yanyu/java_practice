3e0b96df41ee9258ef9628c20d66b9e8ef78f462
付延余 <F-Yanyu@outlook.com>
2023-12-18 20:44:35 +0800

    javaStudy

---
+++ src/main/java/org/example/project/EncryptionAndSecurity/EncryptionAlgorithm.java
@@ -0,0 +1,10 @@
+package org.example.project.EncryptionAndSecurity;
+
+public class EncryptionAlgorithm {
+    public static void main(String[] args) {
+        /**
+         * @编码算法
+         *
+         */
+    }
+}
